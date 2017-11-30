bd55503b75b407e91391b855211491cb1c985802
hellohuyue <407387377@qq.com>
2017-11-30 11:50:22 +0800

    Update FindPeople.py
    加了卡死断开机制

--- FindPeople.py
+++ FindPeople.py
@@ -3,12 +3,15 @@
 import urllib
 import os
 from bs4 import BeautifulSoup
-  
-f = open('F:\j2kly.csv','a')
-i = 1500
+import time
+import socket
+
+timeout = 20
+socket.setdefaulttimeout(timeout)  
+i = 100684881
 resulty = 1
-pos = str(i)
-while (i < 2500):
+while (i < 100693727):
+	pos = str(i)
 	res = urllib.urlopen("http://www.timepill.net/people/" + pos)
 	soup = BeautifulSoup(res,"html.parser")
 	result = str(soup.title.encode('gb18030') + pos)
@@ -18,11 +21,10 @@ while (i < 2500):
 		pass
 	else:
 		resulty = result
+		f = open('F:\j2kly.csv','a')
 		f.write(resulty +'\n')
+		f.close
+	print result
 	i = i + 1
-	print i
-
-f.close
-	
-	
-	
+	time.sleep(0.5)
+print result
